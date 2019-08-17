2272a2258428dd8b78bbf9d59381edfce76bdd2f
Diego Pereira <dhiego_ti@hotmail.com>
2019-08-16 22:16:19 -0300

    Configurando projeto para rodar no ZEID

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -114,7 +116,7 @@ const init = async() => {
 
 init()
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if(err){
         console.log('Não foi possivel iniciar o servidor do Jobify')
     }
