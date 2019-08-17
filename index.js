515c94fa18e5ea9cafa5f9b038998424f5e0832b
Diego Pereira <dhiego_ti@hotmail.com>
2019-08-16 23:38:05 -0300

    Bloqueio do admin

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const path = require('path')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
 
-
+app.use('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost'){
+        next()
+    }else{
+        res.send('Not allowed')
+    }
+})
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
